b36917246144e131b0feb9b890c3a751cfba7fb6
Shyam Gami <shyampatelimp@gmail.com>
2024-03-28 21:32:49 +0530

    loader

--- script.js
+++ script.js
@@ -18,4 +18,9 @@ document.addEventListener("DOMContentLoaded", function () {
             scroller: "body",
         }
     })
+    var loder = document.getElementById("loder");
+
+    window.addEventListener("load", function () {
+        loder.style.display = "none";
+    });
 })
